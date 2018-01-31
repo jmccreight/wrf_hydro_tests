0dba467d0161d4451d14917da58667eb2d3e7976
James McCreight <jamesmcc@ucar.edu>
2018-01-31 15:24:59 -0700

    Environment variable work around for touchy old CircleCi

--- questions/regression.sh
+++ questions/regression.sh
@@ -9,6 +9,47 @@
 ##q Compares: All restart files RESTART, HYDRO_RST, and nudging (if available). 
 ###################################
 
+###################################
+## Clone reference fork into repos directory
+###################################
+## The reference fork can be optional if both $referenceLocalPath and $referenceFork equal ''.
+if [[ -z $referenceLocalPath ]]; then
+    if [[ ! -z $referenceFork ]]; then
+	if [[ -e $refRepoDir ]]; then
+            chmod -R 755 $refRepoDir
+            rm -rf $refRepoDir
+	fi
+	mkdir -p $refRepoDir
+	cd $refRepoDir
+	echo -e "\e[0;49;32m-----------------------------------\e[0m"
+	echo -e "\e[7;49;32mReference fork: $referenceFork\e[0m"
+	echo -e "\e[7;49;32mReference branch/commit: $referenceBranchCommit\e[0m"
+	git clone https://${authInfo}@github.com/$referenceFork $refRepoDir    
+	git checkout $referenceBranchCommit || \
+            { echo "Unsuccessful checkout of $referenceBranchCommit from $referenceFork."; exit 1; }
+	echo -e "\e[0;49;32mRepo in\e[0m `pwd`"
+	echo -e "\e[0;49;32mReference branch:\e[0m    `git rev-parse --abbrev-ref HEAD`"
+	echo -e "\e[0;49;32mReference commit:\e[0m"
+	git log -n1 | cat
+    fi
+else
+    cd $referenceLocalPath
+    echo
+    echo -e "\e[0;49;32m-----------------------------------\e[0m"
+    echo -e "\e[7;49;32mReference fork: LOCAL: `pwd` \e[0m"
+    echo -e "\e[0;49;32mReference branch:\e[0m    `git rev-parse --abbrev-ref HEAD`"
+    gitDiffInd=`git diff-index HEAD -- `
+    if [[ -z $gitDiffInd ]]; then 
+        echo -e "\e[0;49;32mTesting commit:\e[0m"
+        git log -n1 | cat
+    else 
+        echo  -e "\e[0;49;32mTesting uncommitted changes.\e[0m"
+    fi
+    cd - >/dev/null 2>&1
+fi
+
+
+
 ## This should, in theory, never fail. Though could be possible is unexpected/untested
 ## compile options are passed to the reference build.
 echo
