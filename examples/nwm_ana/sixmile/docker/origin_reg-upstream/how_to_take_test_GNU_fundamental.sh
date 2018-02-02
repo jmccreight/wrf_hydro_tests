064dd45710297755662804060b9ae1c5dcd2ed16
James McCreight <jamesmcc@ucar.edu>
2018-02-02 12:23:01 -0700

    Docker examples pull the wrf_hydro_test repo

--- examples/nwm_ana/sixmile/docker/origin_reg-upstream/how_to_take_test_GNU_fundamental.sh
+++ examples/nwm_ana/sixmile/docker/origin_reg-upstream/how_to_take_test_GNU_fundamental.sh
@@ -3,15 +3,16 @@
 docker pull wrfhydro/domains:sixmile
 
 export WRF_HYDRO_TESTS_DIR=/Users/jamesmcc/WRF_Hydro/wrf_hydro_tests/
+export authInfo=${GITHUB_USERNAME}:${GITHUB_AUTHTOKEN}
 
 ## Should always pass or the model is not deterministic.
 docker run -it \
-       -v $WRF_HYDRO_TESTS_DIR:/wrf_hydro_tests \
        -e GITHUB_USERNAME=$GITHUB_USERNAME \
        -e GITHUB_AUTHTOKEN=$GITHUB_AUTHTOKEN \
        wrfhydro/domains:sixmile \
+       /bin/bash -c "git clone https://${authInfo}@github.com/NCAR/wrf_hydro_tests.git /wrf_hydro_tests; \
        /wrf_hydro_tests/take_test.sh \
        /wrf_hydro_tests/examples/nwm_ana/sixmile/docker/origin_reg-upstream/candidate_spec_GNU.sh \
-       fundamental
+       fundamental"
 
 exit $?
