1f8e72d1f17f70a26481cbb3603c53e6adaf4c0e
James McCreight <jamesmcc@ucar.edu>
2018-02-02 12:53:48 -0700

    Got logging back with the docker examples. Printing the machine spec file in boilerplate.

--- examples/nwm_ana/sixmile/docker/origin_reg-upstream/how_to_take_test_GNU_fundamental.sh
+++ examples/nwm_ana/sixmile/docker/origin_reg-upstream/how_to_take_test_GNU_fundamental.sh
@@ -7,12 +7,13 @@ export authInfo=${GITHUB_USERNAME}:${GITHUB_AUTHTOKEN}
 
 ## Should always pass or the model is not deterministic.
 docker run -it \
+       -v `pwd`:/test_specs_logs \
        -e GITHUB_USERNAME=$GITHUB_USERNAME \
        -e GITHUB_AUTHTOKEN=$GITHUB_AUTHTOKEN \
        wrfhydro/domains:sixmile \
        /bin/bash -c "git clone https://${authInfo}@github.com/NCAR/wrf_hydro_tests.git /wrf_hydro_tests; \
        /wrf_hydro_tests/take_test.sh \
-       /wrf_hydro_tests/examples/nwm_ana/sixmile/docker/origin_reg-upstream/candidate_spec_GNU.sh \
+       /test_specs_logs/candidate_spec_GNU.sh \
        fundamental"
 
 exit $?
