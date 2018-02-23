f2c609c6c70e18058485c6afc51e9f541f7f2ad6
James McCreight <jamesmcc@ucar.edu>
2018-02-23 13:20:16 -0700

    Instantly needed parameters to be backwards compatible for all questions

--- questions/run.sh
+++ questions/run.sh
@@ -18,8 +18,12 @@ cp $candidateBinary . || {
     echo -e "Candidate binary not found";
     exit 1;}
 cp $candidateRepoDir/trunk/NDHMS/Run/*TBL . || {
-    echo -e "Candidate parameter tables not found";
-    exit 1;}
+    echo -e "Candidate parameter tables not found in Run/ attempting to find in template/HYDRO ";
+    cp $candidateRepoDir/trunk/NDHMS/template/HYDRO/*TBL . && \
+        cp $candidateRepoDir/trunk/NDHMS/template/NoahMP/*TBL . || {
+        echo -e "Candidate parameter tables not found  find in template/HYDRO ";
+        exit 1;} ;
+}
 
 if [[ -z $WRF_HYDRO_RUN ]]; then source $toolboxDir/mpiRun.sh; fi
 $WRF_HYDRO_RUN $nCoresDefault $candidateBinary question_run $TEST_WALL_TIME $TEST_QUEUE
