66b15927e6279b416390ece156be3c6dfc7f4c99
James McCreight <jamesmcc@ucar.edu>
2018-04-20 15:21:12 -0600

    Moving to cheyenne

--- toolbox/establish_job.py
+++ toolbox/establish_job.py
@@ -1,20 +1,25 @@
-from pprint import pprint
-import re
-import sys
 import os
+#from pprint import pprint
+import re
+import socket
+import sys
 import warnings
+
+
 home = os.path.expanduser("~/")
 sys.path.insert(0, home + '/WRF_Hydro/wrf_hydro_tests/toolbox/')
 from establish_specs import establish_spec, establish_default_files
 
 
-def get_sched_args_from_specs(job_name: str=None,
-                              nnodes: int=None,
-                              nproc: int=None,
-                              run_dir: str=None,
-                              machine_spec_file: str=None,
-                              user_spec_file: str=None,
-                              candidate_spec_file: str=None):
+def get_job_args_from_specs(
+    job_name: str=None,
+    mode: str='r',
+    nnodes: int=None,
+    nproc: int=None,
+    machine_spec_file: str=None,
+    user_spec_file: str=None,
+    candidate_spec_file: str=None
+):
 
     # The candidate comes first and can replace the machine and user specs files.
     # The user spec can overrides parts of the machine spec.
@@ -24,7 +29,7 @@ def get_sched_args_from_specs(job_name: str=None,
     if candidate_spec_file:
         candidate_spec = establish_spec(candidate_spec_file)
     #pprint(candidate_spec)
-        
+    
     machine_spec_file_from_candiate = candidate_spec['wrf_hydro_tests']['machine_spec']
     if machine_spec_file_from_candiate:
         machine_spec_file = machine_spec_file_from_candiate
@@ -38,7 +43,7 @@ def get_sched_args_from_specs(job_name: str=None,
               user_spec_file)
     
     default_user_file, default_machine_file = establish_default_files()
-        
+    
     if not user_spec_file:
         user_spec_file = default_user_file
 
@@ -57,32 +62,54 @@ def get_sched_args_from_specs(job_name: str=None,
     spec.update(candidate_spec)
 
     # Extract relevant information for the scheduler
-    machine_raw = os.path.expandvars('$HOSTNAME')
-    if re.search('cheyenne',machine_raw): machine = 'cheyenne'
-    scheduler_name = spec[machine]['scheduler']['name']
+    machine = socket.gethostname()
+    if re.search('cheyenne',machine):
+        machine = 'cheyenne'
+
+    if machine not in spec.keys():
+        # TODO(JLM): CHAGE DEFAULT MACHINE NAME BACK TO DOCKER, THIS IS JUST FOR TESTING.
+        machine = 'cheyenne'
+        warnings.warn("Machine not found in the machine_spec_file.yaml, using docker.")
+
+    if spec[machine]['scheduler'] is not None:
+        scheduler_name = spec[machine]['scheduler']['name']
+    else:
+        scheduler_name = None
     compiler_name = spec['compiler']
 
     sched_args_dict = {}
+    job_args_dict = {}
     # sad alias for mutable container
     sad = sched_args_dict
-
+    jad = job_args_dict
+    
     # Fomr optional arguments
-    if job_name: sad['job_name']  = job_name
-    if nnodes:   sad['nnodes']= nnodes
-    if nproc:    sad['nproc'] = nproc
+    if job_name:
+        sad['job_name'] = job_name
+    if nnodes:
+        sad['nnodes'] = nnodes
+    if nproc:
+        sad['nproc'] = nproc
+        jad['nproc'] = nproc
 
-    if run_dir:  sad['run_dir']    = os.path.expandvars(run_dir)
-
+    jad['machine'] = machine
+        
     # From spec files.
-    sad['account']    = spec[scheduler_name]['account']
-    sad['email_when'] = spec[scheduler_name]['email']['when']
-    sad['email_who']  = spec[scheduler_name]['email']['who']
-    sad['queue']      = spec['queue']
-    sad['walltime']   = spec['wall_time']
-    sad['ppn']        = spec[machine]['cores_per_node']
-    sad['exe_cmd']    = spec[machine]['exe_cmd'][scheduler_name]
-    sad['modules']    = spec[machine]['modules'][compiler_name]
-    if 'base' in spec[machine]['modules'].keys():
-        sad['modules'] += ' ' + spec[machine]['modules']['base']
+    if scheduler_name is not None: 
+        sad['account']    = spec[scheduler_name]['account']
+        sad['email_when'] = spec[scheduler_name]['email']['when']
+        sad['email_who']  = spec[scheduler_name]['email']['who']
+        sad['queue']      = spec['queue']
+        sad['walltime']   = spec['wall_time']
+        sad['ppn']        = spec[machine]['cores_per_node']
+        jad['exe_cmd']    = spec[machine]['exe_cmd'][scheduler_name]
+    else:
+        jad['exe_cmd']    = spec[machine]['exe_cmd']
 
-    return(sched_args_dict) # = sad
+    if spec[machine]['modules'] is not None: 
+        jad['modules']    = spec[machine]['modules'][compiler_name]
+        if 'base' in spec[machine]['modules'].keys():
+            jad['modules'] += ' ' + spec[machine]['modules']['base']
+
+    jad['scheduler'] = sad
+    return(job_args_dict)
